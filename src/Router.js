30a0aed7e0afe5068312afbeca3ecfa9f96f4d1f
최슬기 <choisk09@gmail.com>
2022-10-16 20:41:26 +0900

    WIP

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,7 @@ import Todo from './pages/Todo';
 
 const Router = () => {
   return (
-    <BrowserRouter basename="/wanted-pre-onboarding-frontend">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/todo" element={<Todo />} />
